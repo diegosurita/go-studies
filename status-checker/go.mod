853841396523bfb60f1cf39d1d45c91da421d732
Diego Surita <suritadev@gmail.com>
2023-09-06 09:06:59 -0300

    learned about go routines and channels

---
+++ status-checker/go.mod
@@ -0,0 +1,3 @@
+module github.com/diegosurita/go-studies/status-checker
+
+go 1.21.0
---
+++ status-checker/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://go.dev",
+		"http://amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		c <- "Link " + link + " might be down!"
+		return
+	}
+
+	c <- link + " is up!"
+}
